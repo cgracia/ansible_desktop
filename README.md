032339959a5d40843f4146f58c81d04f55185442
Carlos Gracia <carlos.gracia.sola@energy-robotics.com>
2024-03-24 16:49:45 +0100

    Rearranging, fixing fuzzy lock

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ansible_desktop
+
+This is a simple ansible playbook to setup a desktop environment on a fresh Ubuntu 22.04 installation.
+
+## Usage
+
+1. Install ansible on your machine
+2. Run the following command to setup the desktop environment:
+```bash
+sudo ansible-pull -U https://github.com/cgracia/ansible_desktop.git
+```
+3. ????
+4. Profit!
---
+++ files/config/i3/scripts/fuzzy_lock.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+i3lock-fancy &
+
+# Brief delay to ensure i3lock starts
+sleep 10
+
+delay=20
+log_file="/tmp/fuzzy_lock.log"
+
+echo "$(date): Lock initiated" >> "$log_file"
+
+while true; do
+    # Check if i3lock is running
+    if pgrep i3lock > /dev/null; then
+        echo "$(date): i3lock is running, waiting for $delay seconds." >> "$log_file"
+        sleep $delay
+
+        # Check again to make sure the system didn't get unlocked while waiting
+        if pgrep i3lock > /dev/null; then
+            echo "$(date): Attempting to turn off the screen." >> "$log_file"
+            xset dpms force off
+        fi
+    else
+        # If i3lock is not running, exit the loop
+        echo "$(date): i3lock is not running. Exiting." >> "$log_file"
+        break
+    fi
+done
