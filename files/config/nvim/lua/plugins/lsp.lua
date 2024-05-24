4bbff0f129885f3eb1b508c4748b867d57bff57a
Carlos Gracia <carlos.gracia.sola@energy-robotics.com>
2024-05-24 09:49:22 +0200

    Autoinstall nvim lsp

--- files/config/nvim/lua/plugins/lsp.lua
+++ files/config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,28 @@
 return {
     {
         'williamboman/mason.nvim',
+        dependencies = {
+          "WhoIsSethDaniel/mason-tool-installer.nvim",
+        },
         config = function()
-            require('mason').setup()
-        end,
+            local mason = require("mason")
+
+            local mason_tool_installer = require("mason-tool-installer")
+
+            -- enable mason and configure icons
+            mason.setup()
+
+            mason_tool_installer.setup({
+              ensure_installed = {
+                "prettier", -- prettier formatter
+                "stylua", -- lua formatter
+                "isort", -- python formatter
+                "black", -- python formatter
+                "pylint", -- python linter
+                "eslint_d", -- js linter
+              },
+            })
+          end,
     },
     {
         'williamboman/mason-lspconfig.nvim',
