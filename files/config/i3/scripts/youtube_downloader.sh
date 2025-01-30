ba280e5a38c2061e6d72c0bdd0bc499453b5e5cc
Carlos Gracia <carlos.gracia.sola@energy-robotics.com>
2025-01-30 19:00:26 +0100

    Add automatically to cmus when downloading

--- files/config/i3/scripts/youtube_downloader.sh
+++ files/config/i3/scripts/youtube_downloader.sh
@@ -6,4 +6,8 @@ dest=~/Music
 
 result=$(yt-dlp -x --audio-format opus --add-metadata --output "$dest/%(uploader)s/%(title)s.%(ext)s" "$url")
 
+if [ $? -eq 0 ]; then
+    cmus-remote -C "add $dest"
+fi
+
 notify-send "Youtube downloader finished with result $result"
