6e602793487f95efc7191b43759904db9c894315
Carlos Gracia <carlos.gracia.sola@energy-robotics.com>
2025-01-30 19:05:04 +0100

    Update monitor names for moving between monitors

--- files/config/i3/scripts/move_screen.sh
+++ files/config/i3/scripts/move_screen.sh
@@ -2,15 +2,12 @@
 
 monitor="eDP-1"
 monitor2="HDMI-2"
-monitor3="DP2-1"
-monitor4="DP-1"
-monitor5="DP-3"
+monitor3="DP-1-1"
+monitor4="DP-1-3"
 
 if xrandr | grep "$monitor2 connected"; then
     i3-msg move $1 to output right
 elif xrandr | grep "^$monitor4 connected"; then
-    i3-msg move $1 to output right
-elif xrandr | grep "^$monitor5 connected"; then
     i3-msg move $1 to output right
 else
     notify-send "No external monitor"
