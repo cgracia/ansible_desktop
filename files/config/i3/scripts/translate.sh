05345ee4eac6af80c5bf5ebd9a422be15601e9bd
Carlos Gracia <carlos.gracia.sola@energy-robotics.com>
2025-02-25 17:19:29 +0100

    Fix API key in translate script, move to vault

--- files/config/i3/scripts/translate.sh
+++ files/config/i3/scripts/translate.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+source ~/.secrets/deepl.env
+
 input=$(xclip -o)
 notify-send "Translating $input"
 
 result=$(curl https://api-free.deepl.com/v2/translate \
--d auth_key=9806bad0-7b1a-7611-e5be-6eb3f5bfc4f4:fx \
+-d "auth_key=$DEEPL_API_KEY" \
 -d "text=$input" \
 -d "target_lang=EN-US")
 
+echo $result
+notify-send $result
 language=$(echo $result | jq '.translations[0].detected_source_language')
 notify-send "Detected language: $language"
 text=$(echo $result | jq '.translations[0].text')
