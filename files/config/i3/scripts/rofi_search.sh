57c3c2765aea25fffbb1a2321899ef63e619613b
Carlos Gracia <carlos.gracia.sola@energy-robotics.com>
2025-08-07 11:43:50 +0200

    Update rofi search with chat gpt search and nerdfont icons

--- files/config/i3/scripts/rofi_search.sh
+++ files/config/i3/scripts/rofi_search.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
-OPTIONS="Jira\nConfluence\nGitLab\nGoogle\nAtlas"
+OPTIONS="󰌃  Jira\n  Confluence\n  GitLab\n  Google\n󰭹  ER-ChatGPT\n 󰭹  ChatGPT"
 ATLASSIAN_INSTANCE="energy-robotics.atlassian.net"
 GITLAB_INSTANCE="gitlab.cicd.energy-robotics.com"
+ER_CHATGPT="chat.energy-robotics.net"
+CHATGPT="chatgpt.com"
 
-CHOICE=$(echo -e "$OPTIONS" | rofi -dmenu -i -p "Search in:")
+CHOICE=$(echo -e "$OPTIONS" | rofi -dmenu -i -p " Search in:")
 
 # Get Search Query
 if [ -n "$CHOICE" ]; then
-    QUERY=$(rofi -dmenu -p "Enter search term:")
-    if [ -n "$QUERY" ]; then
-        case "$CHOICE" in
-            "Jira")
-                xdg-open "https://$ATLASSIAN_INSTANCE/issues/?jql=text~\"$QUERY\""
-                ;;
-            "Confluence")
-                xdg-open "https://$ATLASSIAN_INSTANCE/wiki/search?text=$QUERY"
-                ;;
-            "GitLab")
-                xdg-open "https://$GITLAB_INSTANCE/search?search=$QUERY"
-                ;;
-            "Google")
-                xdg-open "https://www.google.com/search?q=$QUERY"
-                ;;
-            "Atlas")
-                xdg-open "https://team.atlassian.com/projects?tql=(search%20LIKE%20%27$QUERY%27)&viewTab=all"
-                ;;
-            *)
-                notify-send "Unknown choice: $CHOICE"
-                ;;
-        esac
-    fi
+  QUERY=$(rofi -dmenu -p "Enter search term:")
+  if [ -n "$QUERY" ]; then
+    case "$CHOICE" in
+    *"Jira")
+      xdg-open "https://$ATLASSIAN_INSTANCE/issues/?jql=text~\"$QUERY\""
+      ;;
+    *"Confluence")
+      xdg-open "https://$ATLASSIAN_INSTANCE/wiki/search?text=$QUERY"
+      ;;
+    *"GitLab")
+      xdg-open "https://$GITLAB_INSTANCE/search?search=$QUERY"
+      ;;
+    *"Google")
+      xdg-open "https://www.google.com/search?q=$QUERY"
+      ;;
+    *"ER-ChatGPT")
+      xdg-open "https://$ER_CHATGPT/?q=$QUERY"
+      ;;
+    *"ChatGPT")
+      xdg-open "https://$CHATGPT/?q=$QUERY"
+      ;;
+    *)
+      notify-send "Unknown choice: $CHOICE" --icon "$HOME/dotfiles/failed_search.png"
+      ;;
+    esac
+  fi
 fi
